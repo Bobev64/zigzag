90df0b1ea95bf237a8d648783878f95e93bc3051
Bobev64 <68569762+Bobev64@users.noreply.github.com>
2021-12-26 00:49:36 -0500

    Color randomizer added, with extra 'feature' of setting your terminal text to a random color!

--- zigzag.c
+++ zigzag.c
@@ -1,18 +1,35 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
 
 int main(void) {
     int upperBound = 10; // Change this to change size of zigzag
     int lowerBound = 1;
     int higherThanTen, currentSize, spaceAmount = 0;
 
+    int numOfColors = 6;
+
+    char colors[6][8] = {
+        "\e[0;31m",
+        "\e[0;32m",
+        "\e[0;33m",
+        "\e[0;34m",
+        "\e[0;35m",
+        "\e[0;36m"
+    };
+
     while(1 == 1){
+        
+        srand(time(0));
+
         // Draws diagonal going 'up' to the specified upperBound
         while(higherThanTen == 0) {
             if (currentSize < upperBound) {
                 for(int i=0; i<spaceAmount; i++){
                     printf(" ");
                 }
-                printf("3\n");
+                int randColor = rand() % numOfColors;
+                printf("%s3\n", colors[randColor]);
                 currentSize++;
                 spaceAmount++;
             } else {
